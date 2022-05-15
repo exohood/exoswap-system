12f8b641738772944f11f9c9b51511edfb479374
JJ Uzumaki <104718343+xpunk7@users.noreply.github.com>
2022-05-15 14:38:21 -0400

    Delete airdrop.sh

--- scripts/airdrop.sh
+++
@@ -1,9 +0,0 @@
-echo "deploy begin....."
-
-TF_CMD=node_modules/.bin/truffle-flattener
-
-echo "" >  ./deployments/EXOTokenAirDrop.full.sol
-cat  ./scripts/head.sol >  ./deployments/DegoTokenAirDrop.full.sol
-$TF_CMD ./contracts/airdrop/EXOTokenAirDrop.sol >>  ./deployments/EXOTokenAirDrop.full.sol 
-
-echo "deploy end....."
