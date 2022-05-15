7224a563638a868e0eff1b1d0c4d5c2aae9ec89c
JJ Uzumaki <104718343+xpunk7@users.noreply.github.com>
2022-05-15 14:38:31 -0400

    Create airdrop.sh

---
+++ scripts/airdrop.sh
@@ -0,0 +1,9 @@
+echo "deploy begin....."
+
+TF_CMD=node_modules/.bin/truffle-flattener
+
+echo "" >  ./deployments/EXOTokenAirDrop.full.sol
+cat  ./scripts/head.sol >  ./deployments/EXOTokenAirDrop.full.sol
+$TF_CMD ./contracts/airdrop/EXOTokenAirDrop.sol >>  ./deployments/EXOTokenAirDrop.full.sol 
+
+echo "deploy end....."
