ac7007939e6ea9a23863d0e63b0fff8dc3f3cf9e
JJ Uzumaki <104718343+xpunk7@users.noreply.github.com>
2022-06-29 16:47:28 +0200

    Update airdrop.sh

--- scripts/airdrop.sh
+++ scripts/airdrop.sh
@@ -2,8 +2,8 @@ echo "deploy begin....."
 
 TF_CMD=node_modules/.bin/truffle-flattener
 
-echo "" >  ./deployments/EXOTokenAirDrop.full.sol
-cat  ./scripts/head.sol >  ./deployments/EXOTokenAirDrop.full.sol
-$TF_CMD ./contracts/airdrop/EXOTokenAirDrop.sol >>  ./deployments/EXOTokenAirDrop.full.sol 
+echo "" >  ./deployments/ExoswapAirDrop.full.sol
+cat  ./scripts/head.sol >  ./deployments/ExoswapAirDrop.full.sol
+$TF_CMD ./contracts/airdrop/ExoswapAirDrop.sol >>  ./deployments/ExoswapAirDrop.full.sol 
 
 echo "deploy end....."
